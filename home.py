d32dbc49713605adde8cfd78368736116a76439d
kalyan221 <117138506+kalyan221@users.noreply.github.com>
2022-11-11 12:42:18 +0530

    Add files via upload

---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import home
+
+st.set_page_config(page_title = "Python Classes",page_icon = ":house")
+st.title("Welcome To Python Classes Website")
+st.image("IMG-20220421-WA0011-removebg-preview_1.jpeg")
+st.subheader("Welcome Friends")
+st.text(""" This is the python classes website.
+                    First of all again welcome.
+                    This is the beginning of our journey.""")
